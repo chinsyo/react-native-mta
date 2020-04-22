e7728d074a3b684ba62dea98ea8fae735a585558
Chinsyo <chinsyo@sina.cn>
2020-04-22 15:51:18 +0800

    bump to v0.0.9

--- ios/RCTMta/RCTMta.m
+++ ios/RCTMta/RCTMta.m
@@ -11,12 +11,6 @@
 
 RCT_EXPORT_MODULE()
 
-RCT_EXPORT_METHOD(sampleMethod:(NSString *)stringArgument numberParameter:(nonnull NSNumber *)numberArgument callback:(RCTResponseSenderBlock)callback)
-{
-    // TODO: Implement some actually useful functionality
-    callback(@[[NSString stringWithFormat: @"numberArgument: %@ stringArgument: %@", numberArgument, stringArgument]]);
-}
-
 RCT_EXPORT_METHOD(startWithAppKey:(NSString *)key debug:(BOOL)debug) {
     
     [MTAConfig getInstance].debugEnable = debug;
